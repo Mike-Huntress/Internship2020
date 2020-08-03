c6bf497253aeea5161e5685171fc6af0b90973de
Rogerio Junior <rjunior@mit.edu>
2020-08-03 11:37:51 -0400

    credentials handled

--- BasicSetup/Credentials.py
+++ BasicSetup/Credentials.py
@@ -1,5 +1,12 @@
 from BasicSetupUtilities.CredentialsStoreBuilder import DataSourceCredentials
+import json
 
+myCredentials_filename = "credentials_Rogerio.Guimaraes.json"
 
-DataSourceCredentials().addCredentials("datastream", "Your Username", "Your Password")
+with open(myCredentials_filename) as f:
+    credentials = json.load(f)
+    username = credentials['username']
+    password = credentials['password']
+
+DataSourceCredentials().addCredentials("datastream", username, password)
 DataSourceCredentials().readCredentials("datastream").print()
